d48517b76f92141fdfafe77368a975fc21186132
David Lorenz <info@activenode.de>
2024-08-05 22:12:03 +0200

    removed css mods

--- app/test/page.js
+++ app/test/page.js
@@ -1,19 +1,8 @@
-import * as myFooClasses from './foo.module.css';
-import * as myScssClasses from './special.module.scss';
-import * as myClasses from './test.module.css';
 
 export default function TestPage() {
   return (
-    <main className={myClasses.test}>
+    <main>
       Ich bin die test page.js
-
-      <div className={myFooClasses.test}>
-        Ich müsste Fettschrift sein
-      </div>
-
-      <div className={myScssClasses.special}>
-        Ich sollte 20px font-size haben
-      </div>
     </main>
   );
 }
