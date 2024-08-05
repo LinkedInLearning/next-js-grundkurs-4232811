397a240ffa4424b3bbff852b9a2f6ea700636b62
David Lorenz <info@activenode.de>
2024-08-05 22:10:59 +0200

    up

--- app/test/page.js
+++ app/test/page.js
@@ -1,9 +1,19 @@
-import './test.css';
+import * as myFooClasses from './foo.module.css';
+import * as myScssClasses from './special.module.scss';
+import * as myClasses from './test.module.css';
 
 export default function TestPage() {
   return (
-    <main className="test">
+    <main className={myClasses.test}>
       Ich bin die test page.js
+
+      <div className={myFooClasses.test}>
+        Ich müsste Fettschrift sein
+      </div>
+
+      <div className={myScssClasses.special}>
+        Ich sollte 20px font-size haben
+      </div>
     </main>
   );
 }
