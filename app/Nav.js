29539acf399dcfe6a65c6d597920dc3079783e8b
David Lorenz <info@activenode.de>
2024-08-06 17:13:22 +0200

    nav added

--- app/Nav.js
+++ app/Nav.js
@@ -1,3 +1,14 @@
+import Link from "next/link";
+
 export function Nav() {
-    return <nav>Hallo, ich bin die Nav Component</nav>
+    return <nav className="flex gap-3">
+
+        <Link href='/' className="rounded-lg bg-slate-800 text-white p-4 block underline">
+            Home
+        </Link>
+
+        <Link href='/blog' className="rounded-lg bg-slate-800 text-white p-4 block underline">
+            Blog
+        </Link>
+    </nav>
 }
