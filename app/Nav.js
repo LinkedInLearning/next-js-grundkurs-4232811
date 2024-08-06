3344b943505aae21bcaaaad3b8cb205350303efd
David Lorenz <info@activenode.de>
2024-08-06 17:16:16 +0200

    prettier

--- app/Nav.js
+++ app/Nav.js
@@ -1,14 +1,19 @@
 import Link from "next/link";
 
 export function Nav() {
-    return <nav className="flex gap-3">
+  return (
+    <nav className="flex gap-3">
+      <Link
+        href="/"
+        className="rounded-lg bg-slate-800 text-white p-4 block underline">
+        Home
+      </Link>
 
-        <Link href='/' className="rounded-lg bg-slate-800 text-white p-4 block underline">
-            Home
-        </Link>
-
-        <Link href='/blog' className="rounded-lg bg-slate-800 text-white p-4 block underline">
-            Blog
-        </Link>
+      <Link
+        href="/blog"
+        className="rounded-lg bg-slate-800 text-white p-4 block underline">
+        Blog
+      </Link>
     </nav>
+  );
 }
