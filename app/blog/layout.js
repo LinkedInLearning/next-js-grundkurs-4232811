da71198d1e811330c9e29e007a6491033d36e5ac
David Lorenz <info@activenode.de>
2024-08-06 21:22:17 +0200

    fts

--- app/blog/layout.js
+++ app/blog/layout.js
@@ -1,10 +1,30 @@
-export default function BlogLayout({ children }) {
+import { promises as fs } from 'fs';
+import Link from 'next/link';
+
+export default async function BlogLayout({ children }) {
+  const jsonContent = await fs.readFile(
+    process.cwd() +
+    '/app/blog/articles.json',
+    'utf-8'
+  )
+
+  const articles = JSON.parse(jsonContent).slice(0, 3);
+
   return <div>
     {children}
 
     <section className="mt-8 border-t pt-5">
-      <h2 className="font-bold">Neueste Artikel</h2>
-      <p>Hier sollen die neuen Artikel stehen</p>
+      <h2 className="font-bold text-xl mb-3">Neueste Artikel</h2>
+
+      <ul className='flex flex-col gap-2'>
+        {articles.map((article, i) => {
+          return <li key={i}>
+            <Link href={`/blog/${i}`} className='underline text-orange-700'>
+              {article.title}
+            </Link>
+          </li>
+        })}
+      </ul>
     </section>
   </div>;
 }
--- app/blog/page.js
+++ app/blog/page.js
@@ -1,8 +1,26 @@
+import { promises as fs } from 'fs';
+import Link from 'next/link';
 
-export default function Blog() {
+export default async function Blog() {
+  const jsonContent = await fs.readFile(
+    process.cwd() +
+    '/app/blog/articles.json',
+    'utf-8'
+  )
+
+  const articles = JSON.parse(jsonContent);
+
   return (
     <section>
-      Hier befindet sich der Blog
+      <ul className='flex flex-col gap-2'>
+        {articles.map((article, i) => {
+          return <li key={i}>
+            <Link href={`/blog/${i}`} className='underline'>
+              {article.title}
+            </Link>
+          </li>
+        })}
+      </ul>
     </section>
   );
 }
