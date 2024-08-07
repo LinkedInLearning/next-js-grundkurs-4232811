b53e168cf4b82ee85f9697de0c05408ded580c1a
David Lorenz <info@activenode.de>
2024-08-07 08:58:12 +0200

    img

--- app/blog/[index]/page.js
+++ app/blog/[index]/page.js
@@ -1,4 +1,5 @@
 import { promises as fs } from 'fs';
+import Image from 'next/image';
 
 export default async function ArticleView({ params }) {
   const jsonContent = await fs.readFile(
@@ -11,6 +12,10 @@ export default async function ArticleView({ params }) {
   const article = articles[params.index];
 
   return <article>
+    {article.image && <div className='relative aspect-video mb-3'>
+      <Image fill src={article.image} quality={85} />
+    </div>}
+
     <h1 className='text-2xl font-bold mb-4'>{article.title}</h1>
     <section>{article.content}</section>
   </article>
