de44fb05c5cf9387d0bdf0f60bf0ef04dcbd59a4
David Lorenz <info@activenode.de>
2024-08-07 22:47:25 +0200

    fix

--- app/basic-auth/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET(request: Request) {
-  return new Response("User und Passwort angeben!", {
-    status: 401,
-    headers: {
-      "WWW-Authenticate": "Basic realm='privat'",
-    },
-  });
-}
