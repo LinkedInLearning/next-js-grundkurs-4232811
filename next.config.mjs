a9867496526b11e56947fa3efb96d9bffefb8271
David Lorenz <info@activenode.de>
2024-08-08 09:00:10 +0200

    07_01

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,28 @@
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['sequelize', 'mysql2']
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/special',
+        destination: '/blog/3'
+      },
+      {
+        source: '/alter-beitrag',
+        destination: 'http://localhost:3000/blog/1'
+      }
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/b/:id',
+        destination: '/blog/:id',
+        permanent: true
+      }
+    ]
+  },
 };
 
 export default nextConfig;
